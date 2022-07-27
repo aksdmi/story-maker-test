022ee06af0bde22b85d0ef8a591ebc45e3e46272
aksdmi <aksdmi@gmail.com>
2022-07-28 00:36:19 +0300

    Решение не до конца

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ class TextFormatter:
         self.positions = positions
         self.stop_words = stop_words if stop_words else []
 
+
+               
+
     def get_as_list(self, username: str) -> list:
         """
         Метод возвращает правильно сформированный список слов итогового текста.
@@ -20,7 +23,32 @@ class TextFormatter:
         :param username: Имя пользователя
         :return: Список слов в правильном порядке
         """
-        pass
+        resdict = {}
+        for i in range(len(self.positions)):
+            val = self.positions[i]
+            val2 = self.strings[i]
+            
+            if val2 in self.stop_words:
+                continue
+
+            if val2 == "{username}":
+                val2 = username
+
+
+            if isinstance(val, list):
+                for j in range(len(val)):
+                    resdict[val[j]] = val2    
+            else:
+                # reslist.insert(val, self.strings[i])
+                resdict[val] = val2
+        
+        # for x in sorted(resdict.keys()):
+        #     print(resdict[x])
+
+
+        return [resdict[x] for x in sorted(resdict.keys())]
+
+
 
     def get_as_text(self, username: str) -> str:
         """
@@ -32,12 +60,51 @@ class TextFormatter:
         :param username: Имя пользователя
         :return: Текст, отформатированный согласно условиям задачи
         """
-        pass
+        
+        strlist = self.get_as_list(username)
+
+        was_sign = False
+        was_dot = False
+
+        stop_signs = ('.', '!', '?', ',')
+
+        strlist2 = []
+
+        if not strlist:
+            return ''
+
+        first = True
+
+        for mstr in strlist:
+            if mstr in stop_signs:
+                was_sign = True
+                if mstr == '.':
+                    was_dot = True
+                strlist2.append(mstr)
+            elif was_sign:
+                if was_dot:
+                    strlist2.append(f' {mstr.title()}')
+                    was_dot = False
+                else:
+                    strlist2.append(mstr)
+                    was_sign = False
+            else:
+                if first:
+                    strlist2.append(mstr.title())
+                    first = False
+                else:
+                    strlist2.append(mstr)
+        
+        return ' '.join(strlist2)
 
 
 formatter = TextFormatter(list_of_strings, list_of_strings_positions, stop_words)
 
-arguments_parser = argparse.ArgumentParser(prog="python main.py", description="Консольный рассказчик.")
+# print(formatter.get_as_list('Dima'))
+print(formatter.get_as_text('Dima'))
+
+
+arguments_parser = argparse.ArgumentParser(prog="python3 main.py", description="Консольный рассказчик.")
 arguments_parser.add_argument('-u',
                               '--username',
                               action='store',
@@ -47,3 +114,4 @@ arguments = arguments_parser.parse_args()
 
 if arguments.username:
     print(formatter.get_as_text(arguments.username))
+    
